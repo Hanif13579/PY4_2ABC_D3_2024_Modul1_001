3eb875cd6d53d22e85a9e74ddad6534b4c2e7b85
hanif13579 <hanifidin.ibrahim.tif24@polban.ac.id>
2026-02-10 09:36:54 +0700

    menambahkan step untuk counter(task 1), menambahkan 5 riwayat terakhir(task 2 ), menambahkan warna pada counter dan decrement, menambahkan konfirmasi pada tombol reset

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter SRP Demo',
+      title: 'LogBook App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // Memanggil View yang sudah kita buat tadi
       home: const CounterView(),
     );
   }
